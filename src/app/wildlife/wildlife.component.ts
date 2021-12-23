01d12af022a7cdcf75a6aa7e4208e14966e792c1
afridhasuhaina <47314310+afridhasuhaina@users.noreply.github.com>
2021-12-23 13:40:31 +0530

    Add files via upload

---
+++ src/app/wildlife/wildlife.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wildlife',
+  templateUrl: './wildlife.component.html',
+  styleUrls: ['./wildlife.component.css']
+})
+export class WildlifeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
